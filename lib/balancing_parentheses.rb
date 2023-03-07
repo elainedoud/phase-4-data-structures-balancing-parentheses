4425ca17201094c8f6aa597e04a5d06ce4ef3dbd
Elaine <elainedoud@gmail.com>
2023-03-07 13:00:16 -0500

    commit tree 660e5b9aaa65698ed0d4a6d3a7edc14ec7925d20

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,20 @@
 require_relative './stack'
 
+def balancing_parentheses(str)
+    stack = Stack.new
+
+    str.chars.each do |c|
+        if stack.peek == '(' && c == ')'
+            stack.pop
+        else 
+            stack.push(c)
+        end
+    end
+    stack.size
+end
+
 # your code here
+# put the string into the stack
+# determine if there is a ( , if there is a corresponding ) in the stack
+# and vice versa
+# keep count of any ( or ) does is not part of a pair
